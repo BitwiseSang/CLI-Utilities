8476523cc6de5e580a65dc86e0aa6bebc2d86aa1
BitwiseSang <135348362+BitwiseSang@users.noreply.github.com>
2025-09-06 22:19:40 +0300

    refactor: improve code usability

--- commit-scripts/obsidian_vault_backup.sh
+++ commit-scripts/obsidian_vault_backup.sh
@@ -13,6 +13,10 @@ if ! cd "$target_dir"; then
   exit 1
 fi
 
+# Downloading the custom notification sound
+mkdir -p ~/.sounds
+wget -nc -O ~/.sounds/melodic.mp3 https://cdn.pixabay.com/audio/2025/06/02/audio_f91dce208a.mp3
+
 # Pushing and logging
 git add .
 if [ $? -eq 0 ]; then
@@ -21,22 +25,21 @@ if [ $? -eq 0 ]; then
     git push
     if [ $? -eq 0 ]; then
       echo "Push completed successfully."
+
+      # Play custom sound after commit using paplay
+      paplay ~/.sounds/melodic.mp3
+      # Displaying custom notification using zenity
+      zenity --info --title="Vault Backup" --text="Successfully committed vault backup!\nTime: $time \nDate: $date" --width=200 --height=100
     else
       echo "Git push failed."
+
+      zenity --info --title="Error!" --text="An error occured. Failed to push.\nRerun the script or push manually by typing 'git push'" --width=200 --height=100
     fi
   else
     echo "Commit failed."
+
+    zenity --info --title="Error!" --text="An error occured. Failed to commit." --width=200 --height=100
   fi
 else
   echo "Add failed."
 fi
-
-# Downloading the custom notification sound
-mkdir -p ~/.sounds
-wget -nc -O ~/.sounds/melodic.mp3 https://cdn.pixabay.com/audio/2025/06/02/audio_f91dce208a.mp3
-
-# Custom notifications for linux-based systems.
-# Play custom sound after commit using paplay
-paplay ~/.sounds/melodic.mp3
-# Displaying custom notification using zenity
-zenity --info --title="Vault Backup" --text="Successfully committed vault backup!\nTime: $time \nDate: $date" --width=200 --height=100
